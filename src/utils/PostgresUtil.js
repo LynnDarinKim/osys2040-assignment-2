9878c04e30655220f75a8373769d612358d63625
LynnDarinKim <darinkim1128@gmail.com>
2019-04-08 17:29:25 -0300

    push

--- src/utils/PostgresUtil.js
+++ src/utils/PostgresUtil.js
@@ -1,8 +1,13 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  // ssl: true,
+ //connectionString: process.env.DATABASE_URL,
+ //ssl: true,
+	 user: 'darinkim',
+	 host: 'localhost',
+	 database: 'darinkim',
+	 password: '901128',
+	 port: '5432',
 })
 
 module.exports = {
